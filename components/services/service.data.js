47e54fb8048725c2b1f4420108c77768959aa590
Chandra Septian <cst_dev155@unitedtractors.com>
2023-01-03 15:27:37 +0700

    chore: add service dummy data with json

---
+++ components/services/service.data.js
@@ -0,0 +1,23 @@
+export const listServices = [
+  {
+    description:
+      "Creating mobile applications with several different frameworks.",
+    framework: ["React Native", "Flutter", "Kotlin"],
+    id: 1,
+    title: "Mobile Developer",
+  },
+  {
+    description:
+      "Creating a responsive and attractive website",
+    framework: ["React", "PWA", "Tailwind"],
+    id: 2,
+    title: "Web Developer",
+  },
+  {
+    description:
+      "Configuring pipelines for CI/CD processes",
+    framework: ["Azure", "Docker"],
+    id: 3,
+    title: "DevOps",
+  },
+];
