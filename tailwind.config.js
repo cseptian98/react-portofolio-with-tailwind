3ee1d3c273dbceec2a2ccaf914216b37fd948019
Chandra Septian <cst_dev155@unitedtractors.com>
2023-01-17 16:01:51 +0700

    style: add base color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
@@ -10,6 +12,16 @@ module.exports = {
       fontFamily: {
         burtons: "burtons",
       },
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      "primary-dark": "#222831",
+      "second-dark": "#393E46",
+      "primary-light": "#EEEEEE",
+      "second-light": "#00ADB5",
+      sky: colors.sky,
+      yellow: colors.yellow,
     },
   },
   plugins: [],
