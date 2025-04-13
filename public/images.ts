fafe8ceb003e054a90da780ca808a1438cedc79c
cseptian <cst_dev155@unitedtractors.com>
2025-04-14 05:53:36 +0700

    🍱: Add new asset icons for tech list

--- public/images.ts
+++ public/images.ts
@@ -1,31 +1,39 @@
 import arkamaya from "@public/arkamaya.png";
+import astro from "@public/icons/astro.png";
 import avatar from "@public/avatar.png";
+import axios from "@public/icons/Axios.png";
 import azure from "@public/icons/azure.webp";
 import dicoding from "@public/dicoding.png";
+import docker from "@public/icons/docker.webp";
+import dotnet from "@public/icons/dotnet.webp";
+import github from "@public/icons/github.png";
+import kubb from "@public/icons/kubb.png";
+import kubernetes from "@public/icons/kubernetes.png";
+import nextjs from "@public/icons/nextjs.png";
 import postgre from "@public/icons/postgre.png";
 import react from "@public/icons/react.webp";
 import redux from "@public/icons/redux.webp";
 import tanstack from "@public/icons/tanstack.png";
 import ut from "@public/ut.png";
 import vite from "@public/icons/vite.png";
-import axios from "@public/icons/Axios.png";
-import dotnet from "@public/icons/dotnet.webp";
-import github from "@public/icons/github.png";
-import kubb from "@public/icons/kubb.png";
 
 export {
   arkamaya,
+  astro,
   avatar,
+  axios,
   azure,
   dicoding,
+  docker,
+  dotnet,
+  github,
+  kubb,
+  kubernetes,
+  nextjs,
   postgre,
   react,
   redux,
   tanstack,
   ut,
   vite,
-  axios,
-  dotnet,
-  github,
-  kubb
 };
